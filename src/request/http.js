705f671a030917a38eda6855811ddff3f0fbdf9a
a1237877 <487333601@qq.com>
2019-07-18 17:27:40 +0800

    !

---
+++ src/request/http.js
@@ -0,0 +1,60 @@
+import axios from 'axios'
+import { Toast } from 'mand-mobile'
+import { stringify } from 'qs'
+import store from '../store/index'
+
+let tmpTrip = store.state.trip
+axios.defaults.timeout = 10000
+axios.defaults.withCredentials = false //跨域请求时需要使用凭证 默认否 不使用
+
+//给请求添加拦截
+axios.interceptors.request.use(
+  config => {
+    //判断每次发送请求之前 vuex 中是否存在token
+    // 如果存在 则统一在http请求的header上都加上token 这样后台能根据token判断用户当前是否是登录状态
+    //即使本地存在token 也有可能token是过期的，所以在拦截器中要返回状态进行判断
+    //const token = store.state.token
+    //token 存在并且 (config.header.Authorization = token)
+    config.header['Content-Type'] = 'application/x-www-form-urlencoded'
+    return config
+  },
+  error => {
+    Toast.failed('请求过程出错')
+    return Promise.error(error)
+  }
+)
+//响应拦截器
+axios.interceptors.response.use(function(response){
+  //对响应数据进行操作
+  return response
+},function(error){
+  Toast.failed('响应过程出错')
+  return Promise.reject(error)
+})
+
+var request = (options) => {
+  //每次请求传入当前用户的id
+  if(tmpTrip.user){
+    if(options.body){
+      options.body.userId = tmpTrip.user.userId
+    }
+    if(options.params){
+      options.params.userId = tmpTrip.user.userId
+    }
+  }
+  //表单传值 参数格式化
+  return axios.request({
+    url:`http://localhost:3000${options.url}`,
+    method:options.method,
+    data:options.body,
+    params:options.params
+  }).then(response => {
+    return response
+  },err => {
+    Toast.failed(err.message)
+    throw err
+  }).catch((error)=>{
+    Toast.failed('请求失败')
+    throw error
+  })
+}
